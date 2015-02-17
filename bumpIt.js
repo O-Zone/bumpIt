fbdb31d4d0b7b2b7b75e5dbf653a226460697023
Hasse Feldthaus <hafe@kb.dk>
2015-02-17 17:48:12 +0100

    Added enforced version, and other minor changes.

--- bumpIt.js
+++ bumpIt.js
@@ -5,13 +5,14 @@ var argv = require('minimist')(process.argv.slice(2));
 var xpath = require('xpath');
 var myDOMParser = require('xmldom').DOMParser;
 
-var VERSIONTYPES = ['major', 'minor', 'maintainence'];
-var versionToBump = (typeof argv.version === 'undefined') ? 2 : argv.version;
+var VERSIONTYPES = ['major', 'minor', 'patch'];
+var versionToBump = (typeof argv.vType === 'undefined') ? 2 : argv.vType;
 var fileName = argv.file || 'manifest.xml';
 var xpathToVersion = argv.xpath || '//module/version';
+var enforcedVersion = argv.version;
 
 if (argv.help || argv.h) {
-    console.log('bumpIt bumps module versions in manifest files.\n\nUsage: bumpIt [--help][--file=<FILENAME>][--version=<0|1|2>][--xpath=<XPATHTOVERSIONNODE>]');
+    console.log('bumpIt bumps module versions in manifest files.\n\nUsage: bumpIt [--help][--file=<FILENAME>][--vType=<0|1|2>][--version=<ENFORCEDVERSION>][--xpath=<XPATHTOVERSIONNODE>\n\nIt HAS to be a version number of the type major.minor.patch]');
 } else {
     fs.readFile(fileName, 'utf-8', function (err, data) {
         if (err) {
@@ -30,8 +31,9 @@ if (argv.help || argv.h) {
         if (versionToBump === 1) {
             version[2] = '0';
         }
-        console.log('New version is ' + version.join('.'));
-        versionNodes[0].firstChild.data = version.join('.');
+        var newVersion = (typeof enforcedVersion !== undefined) ? enforcedVersion : version.join('.');
+        console.log('New version is ' + newVersion);
+        versionNodes[0].firstChild.data = newVersion;
         fs.writeFile(fileName, doc, 'utf-8', function (err) {
             if (err) {
                 console.log(err);
